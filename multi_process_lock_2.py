886303c500644812237b28fef6a63b1b360202da
frrrosty <48432753+frrrosty@users.noreply.github.com>
2019-05-06 14:58:41 +0200

    Add files via upload

--- multi_process_lock_2.py
+++ multi_process_lock_2.py
@@ -1,20 +1,27 @@
 import time
 import multiprocessing
 
-def einzahlung(kontostand):
+def einzahlung(kontostand, lock):
 	for i in range(100):
 		time.sleep(0.01)
+		lock.acquire()
 		kontostand.value = kontostand.value + 1
+		lock.release()
 
-def abheben(kontostand):
+def abheben(kontostand, lock):
 	for i in range(100):
 		time.sleep(0.01)
+		lock.acquire()
 		kontostand.value = kontostand.value - 1
+		lock.release()
 
 if __name__ == '__main__':
 	kontostand = multiprocessing.Value('i', 200)
-	e = multiprocessing.Process(target=einzahlung, args=(kontostand,))
-	a = multiprocessing.Process(target=abheben, args=(kontostand, ))
+
+	lock = multiprocessing.Lock()
+
+	e = multiprocessing.Process(target=einzahlung, args=(kontostand, lock))
+	a = multiprocessing.Process(target=abheben, args=(kontostand, lock))
 
 	e.start()
 	a.start()
